29a8f6b0a0354aad5992e5c487fc9424e25bfdae
gokhan <gokhanckgz@hotmail.com>
2016-12-08 01:36:39 +0300

    "logging"

--- Feelincs/settings.py
+++ Feelincs/settings.py
@@ -113,6 +113,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/user/log/feelincs.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
