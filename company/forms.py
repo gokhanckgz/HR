b8d125fcb24e2716804c577f26313998bbf9576a
gokhan <gokhanckgz@hotmail.com>
2016-12-14 23:18:59 +0300

    "fix shown service for emp. fix"

--- company/forms.py
+++ company/forms.py
@@ -1,7 +1,7 @@
 from django.forms import *
 from .models import Company
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit,Layout,HTML,Field
 from company.models import Benefit
 
 
@@ -12,10 +12,14 @@ class ProfileEditForm(ModelForm):
         self.helper.form_method = 'post'
         self.helper.form_action = 'submit_survey'
         self.helper.add_input(Submit('submit', 'Kaydet'))
-
+        self.helper.layout = Layout(
+            Field('company_name'),
+            Field('image'),
+            HTML("""{% if form.image.value %}<img class="img-profile" src="{{ MEDIA_URL }}{{ form.image.value }}">{% endif %}"""),
+        )
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = ['company_name','image']
 
 
 class ServiceUseForm(ModelForm):
--- supplier/forms.py
+++ supplier/forms.py
@@ -2,7 +2,7 @@ from django.forms import *
 from .models import Supplier
 from company.models import Benefit
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit,Layout,HTML,Field
 
 class ProfileEditForm(ModelForm):
 	def __init__(self, *args, **kwargs):
@@ -11,6 +11,13 @@ class ProfileEditForm(ModelForm):
 		self.helper.form_method = 'post'
 		self.helper.form_action = 'submit_survey'
 		self.helper.add_input(Submit('submit', 'Kaydet'))
+		self.helper.layout = Layout(
+			Field('supplier_name'),
+			Field('supplier_email'),
+			Field('image'),
+			HTML(
+				"""{% if form.image.value %}<img class="img-profile" src="{{ MEDIA_URL }}{{ form.image.value }}">{% endif %}"""),
+		)
 	class Meta:
 		model = Supplier
 		fields = ('supplier_name','supplier_email','image')
