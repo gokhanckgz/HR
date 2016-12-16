ebe42228b94df117ff723b3c7692ae84d7aa5ea4
gokhan <gokhanckgz@hotmail.com>
2016-12-17 00:13:57 +0300

    "urls for employe"

--- employee/urls.py
+++ employee/urls.py
@@ -6,9 +6,7 @@ urlpatterns = [
     url(r'^services/$', services , name="Selections"),
     url(r'^profile/$', profile , name="Profile"),
     url(r'^profile/credit_transfer', credit_transfer , name="credit_transfer"),
-    url(r'^services/service_choose/(\d+)', service_choose, name="service_choose"),
-    url(r'^service_info/service_choose/(\d+)', service_choose, name="service_choose"),
-    url(r'^services/service_leave/(\d+)', service_leave, name="service_leave"),
-    url(r'^service_info/service_leave/(\d+)', service_leave, name="service_leave"),
     url(r'^services/service_info/(\d+)', service_info, name="service_info"),
+    url(r'^services/service_info/service_choose/(\d+)', service_choose, name="service_choose"),
+    url(r'^services/service_info/service_leave/(\d+)', service_leave, name="service_leave"),
 ]
